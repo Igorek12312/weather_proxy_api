0b19018291ff6c02f0a4b25644bad58f1908097c
Igor Kozirev <Igor_kozirev@mail.ru>
2020-08-08 20:28:39 +0300

    Update api.py

--- mainapp/api.py
+++ mainapp/api.py
@@ -8,7 +8,7 @@ import datetime
 class WeatherAPIView(APIView):
     def get(self, request, pk):
         units = 'metric'
-        apikey = 'd3fb8a3c27a99a41a336eb043a341986'
+        apikey = ''
         url = f'http://api.openweathermap.org/data/2.5/weather?{pk}&units={units}&apikey={apikey}'
         response = requests.get(url)
         if response.status_code == 200:
